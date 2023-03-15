1cd0980e6b9fa416fc78adba0d3ab57eeff59ea0
dimahvatit <katorgin.dmitry@gmail.com>
2023-03-15 19:08:54 +0300

    revert some deploy configs

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/image-generation-app/"
+  plugins: [react()]
 })
